5c995395467db3833cd1ddbc70c10823e56a41c2
Unknown <johnbuevich@yahoo.com>
2018-04-02 02:38:44 +0300

    added insert/delete point button

--- web/src/components/List.vue
+++ web/src/components/List.vue
@@ -1,20 +1,33 @@
 <template>
-  <div class="list">
-      <div v-for="r in route.points">
+    <div class="list">
+      <md-list-item md-expand>
+        <span> Добавить </span>
+        <md-list slot="md-expand">
+          <input v-model="index" placeholder="index">
+          <input v-model="lat" placeholder="lat">
+          <input v-model="lon" placeholder="lon">
+          <button v-on:click="add_point" >Добавить</button>
+        </md-list>
+      </md-list-item>
+      <div v-for="(r, index) in route.points">
         <md-list class="list_el">
           <md-list-item md-expand>
             <span class="md-list-item-text">{{ r.lat }} : {{ r.lon }}</span>
             <md-list slot="md-expand">
-              <input v-model="r.lat">
-              <input v-model="r.lon">
+              <button v-on:click="" >Изменить</button>
+              <button v-on:click="delete_point(index)" >Удалить</button>
+              <!--<input v-model="r.lat">-->
+              <!--<input v-model="r.lon">-->
             </md-list>
           </md-list-item>
         </md-list>
       </div>
-  </div>
+    </div>
 </template>
 
 <script>
+  var data = { index: 0 };
+
   export default {
 
     name: 'List',
@@ -24,16 +37,21 @@
         require: true,
       },
     },
-
-    data: () => ({}),
-
-    computed: {
-
+    methods: {
+      delete_point: function (index) {
+        alert(index);
+      },
+      add_point: function () {
+        alert(this.index);
+        alert(this.lat);
+      }
     },
 
-    mounted() {
-
-    },
+    data: () => ({
+        index: 0,
+        lat: 0,
+        lon: 0
+    }),
 
   };
 </script>
@@ -50,7 +68,11 @@
   width: 320px;
   height: 410px;
   overflow-y: scroll;
-  border: 2px solid rgba(0, 0, 0, 0.12);;
+  border: 2px solid rgba(0, 0, 0, 0.12);
+  position: absolute;
+  left: 75%;
+  top: 20%;
 }
+
 </style>
 
